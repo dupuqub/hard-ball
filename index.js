55714a2bb68414f2c9c8d0bd9176947444d2afea
dupuqub <dupuqub@gmail.com>
2018-01-05 12:45:54 -0500

    CSS root update working

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 
 'use strict'
 
+//..............................................................................
+//
 var foundation = document.querySelector( '#foundation' )
 var board = document.querySelector( '#board' )
-var aspect = { x : 16, y : 9 }
+var aspect = { x : 16 , y : 9 }
 
+//..............................................................................
+//
+var root =
+[
+  '--foundation-w : 0 ;' ,
+  '--foundation-h : 0 ;' ,
+  '--board-w : 0 ;' ,
+  '--board-h : 0 ;' ,
+]
+
+//..............................................................................
+//
+var reroot = _ =>
+{
+  var rerooted = root.reduce( ( a, b ) => a + b )
+  document.styleSheets[ 1 ].rules[ 0 ].style.cssText = rerooted
+}
+
+//..............................................................................
+//
 var resize = _ =>
 {
   var scale = 0.9
@@ -15,12 +37,10 @@ var resize = _ =>
     var height_space = ( window.innerHeight - height_foundation ) / 2
     var width_board = height_foundation / 15 * 20
 
-    foundation.style.width = window.innerWidth + 'px'
-    foundation.style.height = height_foundation + 'px'
-    foundation.style.transform = 'translateY(' + height_space + 'px)'
-
-    board.style.width = width_board * scale + 'px'
-    board.style.height = height_foundation * scale + 'px'
+    root[ 0 ] = '--foundation-w:' + window.innerWidth + 'px;'
+    root[ 1 ] = '--foundation-h:' + height_foundation + 'px;'
+    root[ 2 ] = '--board-w:' + width_board * scale + 'px;'
+    root[ 3 ] = '--board-h:' + height_foundation * scale + 'px;'
   }
   else
   {
@@ -28,16 +48,23 @@ var resize = _ =>
     var width_space = ( window.innerWidth - width_foundation ) / 2
     var width_board = window.innerHeight / 15 * 20
 
-    foundation.style.width = width_foundation + 'px'
-    foundation.style.height = window.innerHeight + 'px'
-    foundation.style.transform = 'translateX(' + width_space + 'px)'
-
-    board.style.width = width_board * scale + 'px'
-    board.style.height = window.innerHeight * scale + 'px'
+    root[ 0 ] = '--foundation-w:' + width_foundation + 'px;'
+    root[ 1 ] = '--foundation-h:' + window.innerHeight + 'px;'
+    root[ 2 ] = '--board-w:' + width_board * scale + 'px;'
+    root[ 3 ] = '--board-h:' + window.innerHeight * scale + 'px;'
   }
 }
 
-resize() // on initialization
+//..............................................................................
+//
+onresize = event =>
+{
+  resize()
+  reroot()
+}
 
-onresize = event => resize() // on every screen resize event
+//..............................................................................
+//
+resize()
+reroot()
 
