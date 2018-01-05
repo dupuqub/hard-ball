202ac1fcd905a93b9b0a416b0aad8dc4f270ac40
dupuqub <dupuqub@gmail.com>
2018-01-05 14:20:16 -0500

    Further development

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 //..............................................................................
 //
 var foundation = document.querySelector( '#foundation' )
-var board = document.querySelector( '#board' )
+var game = document.querySelector( '#game' )
+var game_scale = 0.9
 var aspect = { x : 16 , y : 9 }
 
 //..............................................................................
@@ -13,8 +14,13 @@ var root =
 [
   '--foundation-w : 0 ;' ,
   '--foundation-h : 0 ;' ,
-  '--board-w : 0 ;' ,
-  '--board-h : 0 ;' ,
+
+  '--game-w :       0 ;' ,
+  '--game-h :       0 ;' ,
+  '--game-radius :  calc( var( --foundation-h ) / 20 ) ;' ,
+
+  '--border-full :  calc( var( --foundation-h ) / 400 ) ;' ,
+  '--border-half :  calc( var( --border-full ) / 2 ) ;' ,
 ]
 
 //..............................................................................
@@ -22,6 +28,7 @@ var root =
 var reroot = _ =>
 {
   var rerooted = root.reduce( ( a, b ) => a + b )
+
   document.styleSheets[ 1 ].rules[ 0 ].style.cssText = rerooted
 }
 
@@ -29,29 +36,27 @@ var reroot = _ =>
 //
 var resize = _ =>
 {
-  var scale = 0.9
-
   if( window.innerHeight / aspect.y > window.innerWidth / aspect.x )
   {
     var height_foundation = window.innerWidth / aspect.x * aspect.y
     var height_space = ( window.innerHeight - height_foundation ) / 2
-    var width_board = height_foundation / 15 * 20
+    var width_game = height_foundation / 15 * 20
 
     root[ 0 ] = '--foundation-w:' + window.innerWidth + 'px;'
     root[ 1 ] = '--foundation-h:' + height_foundation + 'px;'
-    root[ 2 ] = '--board-w:' + width_board * scale + 'px;'
-    root[ 3 ] = '--board-h:' + height_foundation * scale + 'px;'
+    root[ 2 ] = '--game-w:' + width_game * game_scale + 'px;'
+    root[ 3 ] = '--game-h:' + height_foundation * game_scale + 'px;'
   }
   else
   {
     var width_foundation = window.innerHeight / aspect.y * aspect.x
     var width_space = ( window.innerWidth - width_foundation ) / 2
-    var width_board = window.innerHeight / 15 * 20
+    var width_game = window.innerHeight / 15 * 20
 
     root[ 0 ] = '--foundation-w:' + width_foundation + 'px;'
     root[ 1 ] = '--foundation-h:' + window.innerHeight + 'px;'
-    root[ 2 ] = '--board-w:' + width_board * scale + 'px;'
-    root[ 3 ] = '--board-h:' + window.innerHeight * scale + 'px;'
+    root[ 2 ] = '--game-w:' + width_game * game_scale + 'px;'
+    root[ 3 ] = '--game-h:' + window.innerHeight * game_scale + 'px;'
   }
 }
 
